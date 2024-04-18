2108e74d897ece185bf4089a39876c38d03c32b5
ThulasiRam987 <thulasiramviolet@gmail.com>
2024-04-18 21:01:09 +0530

    this is my audio cutter app

---
+++ audio_cutter.py
@@ -0,0 +1,20 @@
+from pydub import AudioSegment
+def split_audio(input_file, output_prefix, segment_length_ms):
+    # Load the audio file
+    audio = AudioSegment.from_file(input_file)
+
+    # Calculate the number of segments
+    num_segments = len(audio) // segment_length_ms
+
+    # Split the audio into segments
+    for i in range(num_segments):
+        start_time = i * segment_length_ms
+        end_time = (i + 1) * segment_length_ms
+        segment = audio[start_time:end_time]
+        segment.export(f"{output_prefix}_{i}.wav", format="wav")
+
+# Example usage:
+input_file = "D:\machine_learning\audio_detection\iam_a_rider.wav"
+output_prefix = "263"
+segment_length_ms = 4000  # Length of each segment in milliseconds (5 seconds in this example)
+split_audio(input_file, output_prefix, segment_length_ms)
